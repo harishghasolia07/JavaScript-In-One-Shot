52fc9c39012cf65dec60c05b78344efa20ca895e
Harish Ghasolia <harishghasolia@gmail.com>
2024-03-18 18:57:28 +0530

    first commit

---
+++ matches_closest_contains.js
@@ -0,0 +1,15 @@
+/*
+//Matches--> It is used to check the element is matching with the given css selector or not.
+let id = document.getElementById('my-id');
+
+console.log(id);
+
+console.log(id.matches('.class'))
+console.log(id.matches('.container'))
+
+//  
+console.log(sp1.closest('.container'))
+
+*/
+
+
