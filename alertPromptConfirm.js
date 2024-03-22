1364102d36410fb731dea544e161083665e1d61a
harishghasolia07 <100846446+harishghasolia07@users.noreply.github.com>
2024-03-23 03:01:49 +0530

    Add files via upload

---
+++ alertPromptConfirm.js
@@ -0,0 +1,16 @@
+// Objective: To understand the use of alert, prompt and confirm
+/*
+alert("Enter the value of a ")
+let a = prompt("Enter Here ","500")
+a = Number.parseInt(a) //Converts string to number
+alert("You entered a of type "+typeof(a))
+
+let b = confirm("Are you sure you want to write to the page? ")
+if(b){
+    document.write(a)
+}
+else{
+    document.write("You have not written to the page so please allow!!")
+}
+document.write(a)
+*/
---
+++ errorhandle.js
@@ -0,0 +1,24 @@
+/*
+// if exception schdule code me aati hai then try catch will not handle the exception
+
+setTimeout(() => {
+    console.log("Hacking wifi....please wait....")
+}, 1000);
+
+try{
+    setTimeout(() => {  //this is a schedule code and try catch will not handle the exception
+    console.log(hello)
+    }, 2000)
+}
+catch(err){
+    console.log("Balle balle")
+}
+setTimeout(() => {
+    console.log("Hacking router....please wait....")
+}, 3000);
+
+setTimeout(() => {
+    console.log("Routers hacked....and ip address is: 120.100.50")
+}, 4000);
+
+*/
